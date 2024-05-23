baa58768d6ad8211195a39a7ad4464da5f6a6619
SundarRajan98 <svaithiy@amd.com>
2024-05-23 17:55:47 +0000

    Fixing issue with multiple loaders

--- rocAL/source/pipeline/master_graph.cpp
+++ rocAL/source/pipeline/master_graph.cpp
@@ -1098,10 +1098,7 @@ void MasterGraph::output_routine_multiple_loaders() {
             }*/
             _process_time.start();
             for (auto& graph : _graphs) {
-                graph->schedule();
-            }
-            for (auto& graph : _graphs) {
-                graph->wait();
+                graph->process();
             }
             _process_time.end();
 
